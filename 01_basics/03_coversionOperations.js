ce9611d93a0e221dcc28f5bc5202d9afe72a14c8
Anju Sharma <anjusharma182003@gmail.com>
2025-04-30 16:41:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_coversionOperations.js
@@ -0,0 +1,18 @@
+// let score = "22"
+// console.log(typeof score);
+
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+
+
+
+// let score = false
+// console.log(typeof score);
+
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+
+let isLoggin =1
+let booleanLoggin = Boolean(isLoggin)
+console.log(booleanLoggin);
+
