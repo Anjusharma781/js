2927dce06ce88520aa70f39c599be91da1b395e1
Anju Sharma <anjusharma182003@gmail.com>
2025-04-30 10:13:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using node js...not browser
+
+let name ="sakshi" //string
+let age =67 //number
+let isLoggin = false //boolean
+//null ==>standlone value...reperstantion hai values ka
+//undefined =>
+//symbol => unique
+
+//object 
+ console.log(typeof null);
+ 
