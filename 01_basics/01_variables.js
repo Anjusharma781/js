3e91c81c6c3d3ee36e2a87ded61aeb30f78f4839
Anju Sharma <anjusharma182003@gmail.com>
2025-04-30 10:15:45 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId =12342
+let accountEmail ="anju@gmail.com"
+var accountPassword = "jaipur"
+console.log( accountId);
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountState])
+
+// prefer not to use var
+
+// because of issue of block scope and  functional scope
