0e86acdbba76cd44d6a6379a20f83ac368cca43e
Anju Sharma <anjusharma182003@gmail.com>
2025-05-04 15:11:31 +0000

    05_string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,44 @@
+const name = "anju sharma "
+const repoCont = 50
+// console.log(name+ repoCont +"value" );  //old function//
+// ` :> string interpolation
+// console.log(`hello my name is ${ name} and my  repo count is ${repoCont}`);
+
+
+const gameName = new String("jds-jadm ")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charCodeAt(3));
+
+// const browserType = "mozilla";
+
+// if (browserType.includes("zilla")) {
+//   console.log("Found zilla!");
+// } else {
+//   console.log("No zilla here!");
+// }
+
+const tagline = "MDN - Resources for developers, by developers";
+// console.log(tagline.indexOf("by")); // 20
+
+
+ const newString =gameName.substring(0,4)
+//  console.log(newString);
+
+const anotherString =  gameName.slice(0,4)
+// console.log(anotherString);
+const newStringOne = "    vishali    "
+// console.log(newStringOne.trim());
+
+const url ="https://vishali.com/vishali%40sharma"
+// console.log (url. replace('%40','-'));
+
+// console.log(url.includes('prm'));
+
+console.log(gameName.split('-'));
+
+
+ 
+
