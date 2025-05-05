4bdf50bc4ceed03d25ca55049b48fd05eebbd0c0
Anju Sharma <anjusharma182003@gmail.com>
2025-05-05 14:30:30 +0000

    Math.num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+
+console.log(balance.toString());
+// console.log(typeof balance )
+
+
+const otherNumber = 24.778
+// console.log(otherNumber.toPrecision());
+
+const hunderds =100000000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+//  by default ati hai java script ke sat
+// it sis a library of JS..
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+console.log(Math.random()*10);
+const min = 10
+const max = 20
+
+/*formulaaaaaaaaaaaaaaaa */
+// console.log(Math.floor(Math.random()* (max - min + 1))+ min);
+
+
+
+
+
+
+
+
