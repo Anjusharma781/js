c0ee229a867559cf91b69bd5be0b45a39322bc2f
Anju Sharma <anjusharma182003@gmail.com>
2025-05-11 06:07:48 +0000

    02_scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,53 @@
+
+
+if (true){
+  let a = 10 
+const b = 20 
+var c = 30  
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+function One(){
+   const username = "vishali"
+
+    function Two(){
+      const website = "youtube"
+      // console.log(username);
+      
+  }
+  // console.log(website);
+
+  Two()
+}
+
+One()
+if(true){
+  const username ="vish"
+  if (username === "vish"){
+  const website =" youtube"
+  // console.log(username + website);
+  }
+  
+  // console.log(website);
+}
+// console.log(username);
+
+
+
+// +++++++++++++++++++ intersting  ++++++++++++++++++++++
+ function addOne(num){
+   return num +1
+ }
+ addOne(5)
+
+
+ 
+ const addTwo = function (num){
+  return num + 2
+ }
+addTwo(5)
